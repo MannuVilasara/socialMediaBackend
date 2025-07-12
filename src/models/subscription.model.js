ccf942328f03d959f1444950cd321ca38bdca5b9
MannuVilasara <mannuvilasara@gmail.com>
2025-07-12 22:51:35 +0530

    ✨ feat: Add Subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,23 @@
+import { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+    {
+        subscriber: {
+            type: Schema.Types.ObjectId, // User who is subscribing
+            ref: "User",
+            required: true,
+        },
+        channel: {
+            type: Schema.Types.ObjectId, // User who is being subscribed to
+            ref: "User",
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
+
+export { Subscription };
